6809c266ecca0159c0ed79e84c3191352b425f7a
Chris Chauvin <Chris.Chauvin@mattersight.com>
2016-04-19 16:07:31 -0500

    Appending parse to the path

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'emnNcRVhoszLtAzLWWOdcW7O9TjL4KnN7QH7cDQC',
   masterKey: process.env.MASTER_KEY || 'ELl0QP5W8yL30HmnIECAjSXEmwEL3OoZUt7qVIY4', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
