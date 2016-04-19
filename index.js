5cb4838ecbad2aa4e790c2a19c09f66d4c9a29a9
Chris Chauvin <Chris.Chauvin@mattersight.com>
2016-04-19 15:53:42 -0500

    Attempting localhost hoping there is some firewal config in front

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'emnNcRVhoszLtAzLWWOdcW7O9TjL4KnN7QH7cDQC',
   masterKey: process.env.MASTER_KEY || 'ELl0QP5W8yL30HmnIECAjSXEmwEL3OoZUt7qVIY4', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://workstyledev.herokuapp.com/',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -45,8 +45,8 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-var port = process.env.PORT || 443;
-var httpServer = require('https').createServer(app);
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
